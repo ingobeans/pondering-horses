73fd5cf806422c34af037bf296b630a25590cada
ingobeans <127751491+ingobeans@users.noreply.github.com>
2025-12-14 19:18:05 +0100

    document script.js a bit

--- script.js
+++ script.js
@@ -8,17 +8,29 @@ function resizeCanvas() {
 }
 
 resizeCanvas();
-
 window.addEventListener("resize", resizeCanvas);
 document.addEventListener('DOMContentLoaded', resizeCanvas, false);
 
-
+const snowflake_amt = 100;
 
 let snowflakes = [];
-
-let snowflake_amt = 100;
-
+// populate snowflakes array
 for (let i = 0; i < snowflake_amt; i++) {
+    // Each snowflake moves to the bottom right of the screen, 
+    // but also has a custom angle that it moves towards.
+    // Each snowflake has a unique factor for how much this custom angle affects its movement.
+    // Each snowflakes custom angle can also change over time (following a sine wave), 
+    // and how much the custom angle changes over time is also dependant on a unique factor
+
+    // Each snowflake is stored as an array, with properties being:
+    //    -x
+    //    -y
+    //    -follow custom angle factor
+    //    -custom angle (0 - 3.14/2)
+    //    -gradual custom angle variation factor
+    //    -lifetime (used by gradual custom angle variation factor)
+
+
     snowflakes.push([Math.random() * canvas.width, Math.random() * canvas.height, Math.random(), Math.random() * (3.14 / 2), Math.random() * 7.0, Math.random()]);
 }
 
@@ -44,11 +56,13 @@ function update(now) {
                 s[1] = -30;
             }
         }
+        // move to bottom right
         s[0] += 1 * deltaTime / 1000.0 * 75.0;
         s[1] += 1 * deltaTime / 1000.0 * 75.0;
+        // move by custom angle
         s[0] += s[2] * (Math.cos(s[3] + s[5] * (Math.sin(s[4]) + 1.0) / 2.0)) * deltaTime / 1000.0 * 75.0;
         s[1] += s[2] * (Math.sin(s[3] + s[5] * (Math.sin(s[4]) + 1.0) / 2.0)) * deltaTime / 1000.0 * 75.0;
-
+        // update lifetime
         s[4] += deltaTime / 1000;
 
     });
