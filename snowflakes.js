fec11d24b474628d647fb22c990f2e68d647d4cc
ingobeans <127751491+ingobeans@users.noreply.github.com>
2025-12-15 17:28:21 +0100

    improve snowflakes script

--- snowflakes.js
+++ snowflakes.js
@@ -19,19 +19,18 @@ for (let i = 0; i < snowflake_amt; i++) {
     // Each snowflake moves to the bottom right of the screen, 
     // but also has a custom angle that it moves towards.
     // Each snowflake has a unique factor for how much this custom angle affects its movement.
-    // Each snowflakes custom angle can also change over time (following a sine wave), 
+    // Each snowflake's custom angle can also change over time (following a sine wave), 
     // and how much the custom angle changes over time is also dependant on a unique factor
 
-    // Each snowflake is stored as an array, with properties being:
-    //    -x
-    //    -y
-    //    -follow custom angle factor
-    //    -custom angle (0 - 3.14/2)
-    //    -gradual custom angle variation factor
-    //    -lifetime (used by gradual custom angle variation factor)
-
-
-    snowflakes.push([Math.random() * canvas.width, Math.random() * canvas.height, Math.random(), Math.random() * (3.14 / 2), Math.random() * 7.0, Math.random()]);
+    let new_snowflake = {
+        x: Math.random() * canvas.width,
+        y: Math.random() * canvas.height,
+        followCustomAngleFactor: Math.random(),
+        customAngle: Math.random() * (3.14 / 2),
+        lifetime: Math.random() * 7.0,
+        gradualCustomAngleVariationFactor: Math.random(),
+    };
+    snowflakes.push(new_snowflake);
 }
 
 let lastTime;
@@ -45,25 +44,25 @@ function update(now) {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
     snowflakes.forEach(s => {
-        ctx.drawImage(snowflake, s[0], s[1]);
-        if (s[0] > canvas.width || s[1] > canvas.height) {
+        ctx.drawImage(snowflake, s.x, s.y);
+        if (s.x > canvas.width || s.y > canvas.height) {
             let yAxis = Math.random() > 0.5;
             if (yAxis) {
-                s[0] = -30;
-                s[1] = Math.random() * canvas.height - 30;
+                s.x = -30;
+                s.y = Math.random() * canvas.height - 30;
             } else {
-                s[0] = Math.random() * canvas.width - 30;
-                s[1] = -30;
+                s.x = Math.random() * canvas.width - 30;
+                s.y = -30;
             }
         }
         // move to bottom right
-        s[0] += 1 * deltaTime / 1000.0 * 75.0;
-        s[1] += 1 * deltaTime / 1000.0 * 75.0;
+        s.x += 1 * deltaTime / 1000.0 * 75.0;
+        s.y += 1 * deltaTime / 1000.0 * 75.0;
         // move by custom angle
-        s[0] += s[2] * (Math.cos(s[3] + s[5] * (Math.sin(s[4]) + 1.0) / 2.0)) * deltaTime / 1000.0 * 75.0;
-        s[1] += s[2] * (Math.sin(s[3] + s[5] * (Math.sin(s[4]) + 1.0) / 2.0)) * deltaTime / 1000.0 * 75.0;
+        s.x += s.followCustomAngleFactor * (Math.cos(s.customAngle + s.gradualCustomAngleVariationFactor * (Math.sin(s.lifetime) + 1.0) / 2.0)) * deltaTime / 1000.0 * 75.0;
+        s.y += s.followCustomAngleFactor * (Math.sin(s.customAngle + s.gradualCustomAngleVariationFactor * (Math.sin(s.lifetime) + 1.0) / 2.0)) * deltaTime / 1000.0 * 75.0;
         // update lifetime
-        s[4] += deltaTime / 1000;
+        s.lifetime += deltaTime / 1000;
 
     });
     requestAnimationFrame(update);
