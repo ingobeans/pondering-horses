4a05dbd1756bd161657fd7a59548fad44cb9d2a0
ingobeans <127751491+ingobeans@users.noreply.github.com>
2026-02-03 18:06:11 +0100

    pond: document time tracking js

--- pond.js
+++ pond.js
@@ -3,12 +3,9 @@ let time = localStorage.getItem("pondTime") | 0;
 
 let base = "time spent at the pond: ";
 
-// Source - https://stackoverflow.com/a/7188188
-// Posted by Shef, modified by community. See post 'Timeline' for change history
-// Retrieved 2026-02-03, License - CC BY-SA 3.0
 
+// only update localStorage every 3 seconds or so
 let timeTilLocalstorage = 0;
-
 function trackTime() {
     time += 1;
     timeTilLocalstorage -= 1;
@@ -16,13 +13,19 @@ function trackTime() {
         timeTilLocalstorage = 3;
         localStorage.setItem("pondTime", time)
     }
+
+    // format time!!
     let seconds = time;
-    let minutes = Math.floor(time / 60);
+    let minutes = Math.floor(seconds / 60);
     let hours = Math.floor(minutes / 60);
     let days = Math.floor(hours / 24);
     let formatted = "";
+
+    // parts consists of data of how to format each part of the data. each item contains:
+    //  - value
+    //  - letter
+    //  - modulo (eg, 60 for seconds, 24 for hours)
     let parts = [[days, "d", null], [hours, "h", 24], [minutes, "m", 60], [seconds, "s", 60]];
-    console.log(minutes);
     for (let part of parts) {
         if (part[0] > 0) {
             if (part[2] != null) {
@@ -32,6 +35,10 @@ function trackTime() {
         }
     }
     timeElement.innerHTML = base + formatted
+
+    // Source - https://stackoverflow.com/a/7188188
+    // Posted by Shef, modified by community. See post 'Timeline' for change history
+    // Retrieved 2026-02-03, License - CC BY-SA 3.0
     setTimeout(trackTime, 1000);
 }
 
